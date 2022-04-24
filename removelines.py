546021daa98a8a5a687067fd8213f7e1362bef23
anurag5040 <76868552+anurag5040@users.noreply.github.com>
2022-04-24 09:52:05 +0530

    removed possible errors

--- removelines.py
+++ removelines.py
@@ -1,9 +1,10 @@
 import sys
+
 output=""
-with open("Output.txt") as f:
+with open("output.txt") as f:
     for line in f:
         if not line.isspace():
             output+=line
             
-f= open("Output.txt","w")
-f.write(Output)
+f= open("output.txt","w")
+f.write(output)
