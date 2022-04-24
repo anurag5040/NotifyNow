4d88006ad4cb77de0e3d197874e2744ce9ad9db2
Harshit Chaudhari <60003982+Lucifer2525-jnv@users.noreply.github.com>
2022-04-24 09:20:53 +0530

    Server Deployment Essential changes done
    Added college website to scrap data from there.........
    before this commit----- college website scrapping script was not added to test the working by explicitly changing text contents since college site was not going to update this much frequently..........

--- main.py
+++ main.py
@@ -8,6 +8,29 @@ import telepot
 import time
 
 alive()
+url = "https://academics.mnnit.ac.in/new"
+
+r = requests.get(url)
+htmlContent = r.content
+#print(htmlContent)
+
+
+# Step 2: Parse the HTML
+soup = BeautifulSoup(htmlContent, 'html.parser')
+mydivs = soup.find_all(id="notification-accordion")
+#print(mydivs)
+
+
+#mydivs = soup.find(id="notification-accordion")
+
+file=open("New.txt","w")
+for anu in mydivs:
+    for item in anu.strings:
+        # print(item)
+        file.write(item)
+file.flush()
+file.close
+
 #giving the paths for various required files.....
 HOME_DIR = os.path.abspath(os.pardir)
 
